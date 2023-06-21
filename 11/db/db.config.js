b42abf305100c662a63059ecfbe6587332807983
AmirGil <amir.gil666@gmail.com>
2023-06-21 12:17:54 +0300

    8

--- 11/db/db.config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    HOST:"localhost", //what are those vars
-    USER:"root",
-    PASSWORD:"Noatiger2020",
-    DB:"mysql"
-};
-module.exports= config;
--- README.md
+++
@@ -1,6 +0,0 @@
-# Amir_Gil
-This is an example for Git and GitHub
-
-Hello
-
-Amir Gil
